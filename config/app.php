fbfd669001b9d522167ea68494c46b35a0148feb
kofi <mokomekofi@yahoo.com>
2019-06-29 20:29:10 +0100

    Pte

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Batch' => Mavinoo\LaravelBatch\LaravelBatchFacade::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        //'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
